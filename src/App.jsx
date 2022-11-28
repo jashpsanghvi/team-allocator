7d238426c3639b62237483c91c609ef26e5a1630
Jash Sanghvi <jash.sanghvi99@gmail.com>
2022-11-28 19:12:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
+import Employees from './Employees'
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Employees/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
